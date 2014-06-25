0f05b71abfe4a601d112bed9b538ab1ff448f7ca
Vincent Spiewak <vspiewak@gmail.com>
2014-06-25 19:37:28 +0200

    Add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO user (login,password,email) VALUES ('admin', 'admin', 'admin@mail.me');
+INSERT INTO user (login,password,email) VALUES ('user', 'user', 'user@mail.me');
+INSERT INTO user (login,password,email) VALUES ('user2', 'user2', 'user2@mail.me');
