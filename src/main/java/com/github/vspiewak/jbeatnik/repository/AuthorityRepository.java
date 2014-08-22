0eef29a735fee4823c49beee51399c38c8295b6e
Vincent Spiewak <vspiewak@gmail.com>
2014-08-22 17:06:00 +0200

    Add AuthorityRepository

---
+++ src/main/java/com/github/vspiewak/jbeatnik/repository/AuthorityRepository.java
@@ -0,0 +1,7 @@
+package com.github.vspiewak.jbeatnik.repository;
+
+import com.github.vspiewak.jbeatnik.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
