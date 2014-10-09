5f8d6b98721b618fcf63128aa20b8b7846c82186
Vincent Spiewak <vspiewak@gmail.com>
2014-10-09 12:04:47 +0200

    Fix Mock UserControllerTest

--- src/test/java/com/github/vspiewak/jbeatnik/controller/UserControllerTest.java
+++ src/test/java/com/github/vspiewak/jbeatnik/controller/UserControllerTest.java
@@ -1,11 +1,14 @@
 package com.github.vspiewak.jbeatnik.controller;
 
 import com.github.vspiewak.jbeatnik.Application;
+import com.github.vspiewak.jbeatnik.domain.User;
 import com.github.vspiewak.jbeatnik.service.UserService;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -16,6 +19,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import javax.inject.Inject;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -24,25 +28,36 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebAppConfiguration
 public class UserControllerTest {
 
-    @Inject
+    @Mock
     private UserService userService;
 
     private MockMvc restUserMockMvc;
 
     @Before
     public void setup() {
+
+        MockitoAnnotations.initMocks(this);
         UserController userController = new UserController();
         ReflectionTestUtils.setField(userController, "userService", userService);
         this.restUserMockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+
     }
 
     @Test
-    public void should_return_admin_when_get_users() throws Exception {
+    public void should_return_user_when_get_profile() throws Exception {
+
+        User user = new User();
+        user.setUsername("admin");
+        user.setEmail("mail@me.com");
+
+        when(userService.getAuthenticatedUser()).thenReturn(user);
+
         restUserMockMvc.perform(get("/api/profile")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith("application/json"))
-                .andExpect(jsonPath("$[0].login").value("admin"));
+                .andExpect(jsonPath("$.username").value("admin"));
+
     }
 
 }
